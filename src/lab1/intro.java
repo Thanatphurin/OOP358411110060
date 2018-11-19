5527c0426e76273769ff01cdb55ef18cc227758f
natthida <natthida11060@gmail.com>
2018-11-19 16:36:00 +0700

    Lab 1 commit

--- src/lab1/intro.java
+++ src/lab1/intro.java
@@ -1,4 +1,13 @@
 package lab1;
 
 public class intro {
-}
+    //to do code here
+    public static void main(String[] args) {
+
+        System.out.println("natthida thanatphurin");
+        System.out.println("natthida11060@gmail.com");
+        System.out.println("is");
+    }//main
+
+
+}//class
