eb526f0249ad8c1a7e848c3a38af28ac3cb964d7
natthida <natthida11060@gmail.com>
2018-12-15 15:06:30 +0700

    Lab 2: Loop

---
+++ src/lab2/ex7.java
@@ -0,0 +1,14 @@
+package lab2;
+
+public class ex7 {
+
+
+    public static void main (String [] args) {
+        int n = 1;
+        do {
+            System.out.println("Hello."+n);
+        } while (++n <= 10);
+        }
+
+
+}//class
